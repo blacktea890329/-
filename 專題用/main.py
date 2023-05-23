86776c1fb8bfd621d91bd51b2a8a61cc6ecbbc11
blacktea890329 <108226946+blacktea890329@users.noreply.github.com>
2023-05-23 14:06:15 +0800

    Add files via upload

--- 專題用/main.py
+++ 專題用/main.py
@@ -1,4 +1,4 @@
-from machine import I2C, Pin, PWM
+from machine import I2C, Pin, PWM ,UART,RTC,WDT,reset
 from ds3231_i2c import DS3231_I2C #RTC讀的是16進制
 import utime
 from GET_TIME import get_time #取得時間的函數
@@ -13,6 +13,73 @@ speaker = PWM(Pin(18))
 
 flag1=0
 D=[0,0,0,0] #早 午 晚 睡前
+
+pin = Pin(28, Pin.OUT, Pin.PULL_UP)
+
+x=0
+wifi_ready=0
+uart = machine.UART(1,tx=Pin(8),rx=Pin(9),baudrate=115200)
+led = Pin(25,Pin.OUT)
+
+led_onboard = machine.Pin(14, machine.Pin.OUT)
+pwm=PWM(Pin(15))
+
+# #=======MQTT/Line notify========
+# reset='RESET'
+# ssid = 'SSID,blacktea890329'   #
+# password = 'PSWD,20000329'   #
+# mqtt_server = 'BROKER,mqttgo.io'
+# linetoken="TOKEN,DD5kW806kKEEH8BMQERiouCxyjwNChRNGWcM3T2swOW"
+# topic_sub = 'TOPIC,black'     
+# topic_pub1= 'TOPIC1,black'
+# topic_pub2= ''#第二個PB 依此類推
+# ready='ready'
+# 
+# def sendCMD_waitResp(cmd, uart=uart, timeout=1000):
+#     print(cmd)
+#     uart.write(cmd+'\r\n')
+#     waitResp()
+#    
+# def waitResp(uart=uart, timeout=1000):
+#     global data,wifi_ready
+#     prvMills = utime.ticks_ms()
+#     resp = b""
+#     while (utime.ticks_ms()-prvMills)<timeout:
+#         if uart.any():
+#             resp = b"".join([resp, uart.read(1)])
+#     if resp != b'' :
+#         data=resp
+#         resp = str(resp)
+#         print(resp)
+#         if (resp.find('connect'))>=0:
+#             wifi_ready=1
+#             
+# sendCMD_waitResp(reset)
+# utime.sleep(0.5)
+# sendCMD_waitResp(ssid)
+# utime.sleep(0.1)
+# sendCMD_waitResp(password)
+# utime.sleep(0.1)
+# sendCMD_waitResp(linetoken)
+# utime.sleep(0.1)
+# sendCMD_waitResp(topic_sub)
+# utime.sleep(0.1)
+# sendCMD_waitResp(topic_pub1)
+# utime.sleep(0.1)
+# # sendCMD_waitResp(topic_pub2)#第二個PB 依此類推
+# # utime.sleep(0.1)
+# sendCMD_waitResp(ready)
+# 
+# while (not wifi_ready) :
+#     utime.sleep(0.3)
+#     led.value(1)
+#     print('.')
+#     utime.sleep(0.3)
+#     led.value(0)
+#     print('.')
+#     waitResp()    
+# print('start')
+# utime.sleep(1)
 
 #設定時間
 # current_time = b'\x00\x14\x08\x03\x17\x05\x23' # sec min hour week day mon year 
@@ -31,25 +98,28 @@ def delay():#設定延遲30分鐘
     global goal_h
     global goal_m
     global goal_s
+    goal_h=[0,0,0,0]
+    goal_m=[0,0,0,0]
+    goal_s=[0,0,0,0]
     t = ds.read_time()
     
     #確定小時 把16進位的數轉為10進位
     d=str(hex(t[2]))
     d=d.split("x")
     d=int(d[1])
-    goal_h=d#延遲多久 後面+數字
+    goal_h[0]=d#延遲多久 後面+數字
     
     #確定分鐘 把16進位的數轉為10進位
     d=str(hex(t[1]))
     d=d.split("x")
     d=int(d[1])
-    goal_m=d#延遲多久 後面+數字
+    goal_m[0]=d#延遲多久 後面+數字
     
     #確定秒 把16進位的數轉為10進位
     d=str(hex(t[0]))
     d=d.split("x")
     d=int(d[1])
-    goal_s=d#延遲多久 後面+數字
+    goal_s[0]=d#延遲多久 後面+數字
     
 btn.irq(handler=None) #關閉中斷功能 避免提前拿藥
 delay()
@@ -77,7 +147,7 @@ while True:
 
     print(" Time: %02x:%02x:%02x" %(t[2],t[1],t[0])) #顯示時間 時/分/秒
     print(t[0],t[1],t[2],t[3],t[4],t[5],t[6])
-    if(hour==goal_h and minu==goal_m and secs==goal_s and D[0]==0):
+    if(hour==goal_h[0] and minu==goal_m[0] and secs==goal_s[0] and D[0]==0):
         btn.irq(handler=take) #開啟中斷功能可以拿藥
         while (D[0]==0):#把16進位的數轉為10進位 設定30秒後跳出
             for i in range(0,10):
@@ -96,12 +166,15 @@ while True:
         print(D)
         print("拿藥了")
         rotate()
-        btn.irq(handler=None)
+#         btn.irq(handler=None)
+#         sendCMD_waitResp('MESSAGE,'+"病患已完成服藥")
         break
     if(b==0 and D[0]==1):
         print("沒有拿藥")
         rotate()
         rotate()
         btn.irq(handler=None)
+#         sendCMD_waitResp('MESSAGE,'+"注意！病患尚未服藥")
+#         sendCMD_waitResp('MESSAGE,'+"建議主動聯絡")
         break
     utime.sleep(1)
